11dd3f39c56d623004e26e6e5f5854d6e2e40d4b
Brendon <brendonqueiros@hotmail.com>
2022-05-15 22:45:26 -0300

    adicionando método de exclusão

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { routeInterstate } = require("fontawesome");
 var db = require('./db_connection.js');
+//document html require
 
 router.get('/', (req, res) => {
     db.query('SELECT * FROM clientes', function (error, clientes) {
@@ -9,7 +10,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/incluir', (req, res) => {
-    res.render("incluir.ejs",{cliente:{}})
+     res.render("incluir.ejs",{cliente:{}})
 })
 
 router.post('/incluir', (req, res) => {
@@ -44,6 +45,22 @@ router.post('/editar/:id', function (req, res){
     )
 })
 
+router.get('/excluir/:id', (req, res) => {
+    const query = `SELECT CodCli,Nome,Contato,Cargo FROM clientes WHERE CodCli = "${req.params.id}"`
+    db.query(query, (error,result) => {
+        res.render("excluir.ejs",{cliente:result[0]})
+    })
+})
+
+router.post('/excluir/:id', (req, res) => {
+    const query = `DELETE FROM clientes WHERE CodCli = "${req.params.id}"`
+    db.query(query, (error,result) => {
+        if(error){
+            return console.error(error.message)
+        }
+        res.redirect('/')
+    })
+})
 
 
 
